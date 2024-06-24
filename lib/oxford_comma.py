546b06de075f392cea85e34f3fd681acb541c1c7
Taylor Ng <taylormng95@gmail.com>
2024-06-24 11:16:45 -0400

    completes oxford comma

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        items_without_end = items[0:len(items) - 1]
+        joined_items_without_end = ', '.join(items_without_end)
+        joined_plus_end = [joined_items_without_end, items[len(items) - 1]]
+        return ', and '.join(joined_plus_end)
